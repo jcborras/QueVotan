4aa8a8199eb970b5a37913207a334e49ba944ea4
Juan Carlos Borrás <jcborras@gmail.com>
2017-01-07 01:01:59 +0200

    Fetches data files from Ministerio del Interior

--- bin/common.py
+++ bin/common.py
@@ -2,17 +2,17 @@
 # -*- coding: utf-8; mode: python; -*-
 
 """ Common funcs and vars """
+from datetime import datetime
+from io import BytesIO
+from os import getpid
+from zipfile import is_zipfile, ZipFile
+
+from requests import get, head
 
 from logging import INFO, Formatter, StreamHandler, getLogger
 
-CONGRESO = ['02201606_MESA.zip', '02201512_MESA.zip', '02201111_MESA.zip',
-            '02200803_MESA.zip', '02200403_MESA.zip', ]
 
-SENADO = ['03201606_MESA.zip', '03201512_MESA.zip', '03201111_MESA.zip',
-          '03200803_MESA.zip', '03200403_MESA.zip', ]
-
-
-def get_logger(name):
+def get_logger(name=__file__):
     handler = StreamHandler()
     fmt = "%(asctime)s:%(filename)16s:%(lineno)3d:%(levelname)s: %(message)s"
     handler.setFormatter(Formatter(fmt))
@@ -21,3 +21,32 @@ def get_logger(name):
     logger.addHandler(handler)
     logger.setLevel(INFO)
     return logger
+
+
+logger = get_logger()
+
+
+def fetch_zip(url, dstdir):
+    logger.info('[PID={pid:n}] Fetching {s:s}'.format(pid=getpid(), s=url))
+    t0 = datetime.now()
+    x = get(url)
+    d = datetime.now() - t0
+    logger.info('\t[PID={pid:n}] GET completed after {d:.2f} s.'.format(
+        pid=getpid(), d=d.total_seconds()))
+    logger.info('\t[PID={pid:n}] Content-Type is {b:s}'.format(
+        pid=getpid(), b=x.headers['Content-Type']))
+    try:
+        assert(x.headers['Content-Type'] == 'application/zip')
+    except AssertionError:
+        logger.fatal('[PID={pid:n}] Resource content mismatch: {s:s}\n'.format(
+            pid=getpid(), s=url))
+        return
+    xx = BytesIO(x.content)
+    logger.info('\t[PID={pid:n}] Expected content is {b:b}'.format(
+        pid=getpid(), b=is_zipfile(xx)))
+    assert(is_zipfile(xx))
+    filename = dstdir + url.split('/')[-1]
+    logger.info('\t[PID={pid:n}] Saving to {s:s}'.format(
+        pid=getpid(), s=filename))
+    with open(filename, 'wb') as f:
+        f.write(x.content)
